3a67b82749e253158a9b144cc3abd82e646530ab
Dominik Szymanski <dominik@codehome.pl>
2024-06-19 20:25:50 +0200

    Fix student registration

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -14,6 +15,7 @@ class ProjectController extends Controller
         if(auth()->user()?->type === 'student') {
             return redirect()->route('student.projects.join', $project);
         }
+        Session::put('project_id', $project->id);
         return Inertia::render('Auth/Register', [
             'project' => $project
         ]);
